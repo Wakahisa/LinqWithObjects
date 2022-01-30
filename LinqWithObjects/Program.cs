1145da6d0c040c679c9c48429291d5032c251332
Wakahisa <zerakyo@gmail.com>
2022-01-29 20:54:44 -0600

    Add project files.

---
+++ LinqWithObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqWithObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> mostExpensiveComicDescriptions =
+                from comic in Comic.Catalog
+                where Comic.Prices[comic.Issue] > 500
+                orderby Comic.Prices[comic.Issue] descending
+                select $"{comic} is worth {Comic.Prices[comic.Issue]:c}";
+
+            foreach (string comic in mostExpensiveComicDescriptions)
+            {
+                Console.WriteLine($"{comic}");
+            }
+        }
+    }
+}
